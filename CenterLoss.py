3e3a8ac7e4654c9f11b7345da62d2c0e52787fe9
Jiaxin Gu <jxgu1016@gmail.com>
2018-10-05 10:06:30 +0800

    ready

--- CenterLoss.py
+++ CenterLoss.py
@@ -15,22 +15,22 @@ class CenterLoss(nn.Module):
         feat = feat.view(batch_size, -1)
         # To check the dim of centers and features
         if feat.size(1) != self.feat_dim:
-            raise ValueError("Center's dim: {0} should be equal to input feature's dim: {1}".format(self.feat_dim,feat.size(1)))
-        loss = self.centerlossfunc(feat, label, self.centers)
-        loss /= (batch_size if self.size_average else 1)
+            raise ValueError("Center's dim: {0} should be equal to input feature's \
+                            dim: {1}".format(self.feat_dim,feat.size(1)))
+        loss = self.centerlossfunc(feat, label, self.centers, batch_size if self.size_average else 1)
         return loss
 
 
 class CenterlossFunc(Function):
     @staticmethod
-    def forward(ctx, feature, label, centers):
-        ctx.save_for_backward(feature, label, centers)
+    def forward(ctx, feature, label, centers, batch_size):
+        ctx.save_for_backward(feature, label, centers, batch_size)
         centers_batch = centers.index_select(0, label.long())
-        return (feature - centers_batch).pow(2).sum() / 2.0
+        return (feature - centers_batch).pow(2).sum() / 2.0 / batch_size
 
     @staticmethod
     def backward(ctx, grad_output):
-        feature, label, centers = ctx.saved_tensors
+        feature, label, centers, batch_size = ctx.saved_tensors
         centers_batch = centers.index_select(0, label.long())
         diff = centers_batch - feature
         # init every iteration
@@ -41,7 +41,7 @@ class CenterlossFunc(Function):
         counts = counts.scatter_add_(0, label.long(), ones)
         grad_centers.scatter_add_(0, label.unsqueeze(1).expand(feature.size()).long(), diff)
         grad_centers = grad_centers/counts.view(-1, 1)
-        return - grad_output * diff, None, grad_centers
+        return - grad_output * diff / batch_size, None, grad_centers / batch_size, None
 
 
 def main(test_cuda=False):
